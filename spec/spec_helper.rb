9d609c26acc38ef5dd3b321ed0bf46267e29021b
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-07 13:34:19 -0400

    rspec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rails'
 
 I18n.enforce_available_locales = false
